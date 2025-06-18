084d67089bdb7d9a7de61023dbf9cdd8be17d34e
Likhona Ngwenya <ST10446272@vcconnect.edu.za>
2025-06-18 19:33:20 +0200

    UI Elements

---
+++ app/src/main/java/vcmsa/ci/weatherapp/SplashActivity.kt
@@ -0,0 +1,17 @@
+package vcmsa.ci.weatherapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        
+
+
+    }
+}
